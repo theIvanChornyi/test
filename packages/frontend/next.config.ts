17d4b0cb6847c4ce80a4fcb23bc6dac197aec504
Ivan Chornyi <theivanchornyi@gmail.com>
2024-12-11 14:54:19 +0300

    add config env

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: { BACKEND_URL: process.env.BACKEND_URL, }
 };
 
 export default nextConfig;
