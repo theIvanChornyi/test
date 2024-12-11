1d5601345f12c23b378d80308a3ce8d422123e0d
Ivan Chornyi <theivanchornyi@gmail.com>
2024-12-11 15:20:51 +0300

    add front url env

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   app.enableCors({
-    origin:      true,
+    origin:      [process.env.FRONTEND_URL],
     credentials: true,
   },)
   await app.listen(process.env.PORT || 3000);
